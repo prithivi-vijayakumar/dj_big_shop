48944d41519fd8f3cbf6957631c8b66092683927
prithivi-vijayakuamr <prithivirajan1207@gmail.com>
2024-09-25 19:22:03 +0530

    rest_framework added to settings.py

--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'backend',
-    'rest_framework'
+    'rest_framework',
+    'api_v2'
 ]
 
 MIDDLEWARE = [
@@ -128,5 +129,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 AUTH_USER_MODEL = 'backend.CustomUser'
