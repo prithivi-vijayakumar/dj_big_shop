f4bc506aceeeea0c0f430cccd6309b21247300c9
prithivi-vijayakumar <prithivirajan1207@gmail.com>
2024-11-15 19:41:19 +0530

    Order and Order Item api v2 added

--- api_v2/urls.py
+++ api_v2/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from api_v2.views import UserCreateAPIView, LogoutAPIView, CategoryListView,CurrentUserView,BrandListView,ProductListView,SubCategoryListView,CartView,CartItemView,ClearCartView
+from api_v2.views import UserCreateAPIView, LogoutAPIView, CategoryListView, CurrentUserView, BrandListView, \
+    ProductListView, SubCategoryListView, CartView, CartItemView, ClearCartView, OrderListView, OrderDetailView
 from rest_framework.authtoken.views import obtain_auth_token
 from api_v2.views import UserCreateAPIView, CustomAuthToken
 
@@ -22,4 +23,6 @@ path('token', obtain_auth_token, name='api_token_auth'),
     path('cart/', CartView.as_view(), name='cart'),
     path('cart/item/', CartItemView.as_view(), name='cart-item'),
     path('clear-cart', ClearCartView.as_view(), name='clear-cart'),
+    path('orders/', OrderListView.as_view(), name='order-list'),
+    path('orders/<int:pk>/', OrderDetailView.as_view(), name='order-detail'),
 ]
