c1cc7ee4353974edc7cd98a323de4f491bbd3e2d
prithivi-vijayakumar <prithivirajan1207@gmail.com>
2025-01-17 12:43:06 +0530

    frontend -> login.html added

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,8 +1,10 @@
+from django.contrib.auth import login
 from django.urls import path
 from frontend.views import forget_password, home
 
 urlpatterns = [
     path('', home),
     path('/', home, name='home'),
+    path('login', login, name='login'),
     path('forget_password', forget_password),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -3,5 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'frontend/home.html')
+def login(request):
+    return render(request, 'frontend/auth/login.html')
 def forget_password(request):
     return render(request, 'frontend/forget_password.html')
