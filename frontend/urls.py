ebf3465303f02caca21573283907d43c80710e2d
prithivi-vijayakumar <prithivirajan1207@gmail.com>
2025-01-18 10:39:43 +0530

    frontend -> register.html added

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,11 +1,11 @@
 from django.contrib.auth import login
 from django.urls import path
-from frontend.views import forget_password, home, register
+from frontend.views import forget_password, home, register, login_page
 
 urlpatterns = [
     path('', home),
     path('/', home, name='home'),
-    path('login', login, name='login'),
+    path('login', login_page, name='login'),
     path('register', register, name='register'),
     path('forget_password', forget_password),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'frontend/home.html')
-def login(request):
+def login_page(request):
     return render(request, 'frontend/auth/login.html')
 def register(request):
     return render(request, 'frontend/auth/register.html')
 def forget_password(request):
-    return render(request, 'frontend/forget_password.html')
+    return render(request, 'frontend/auth/forget_password.html')
